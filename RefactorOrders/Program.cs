a9ba991de753a6a0aa99938d19d89fb34887e58c
Khushi Jain <khushijain0076@gmail.com>
2026-08-27 23:22:56 +0530

    remove comments

--- RefactorOrders/Program.cs
+++ RefactorOrders/Program.cs
@@ -5,8 +5,7 @@ using RefactorOrders.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddLogging();
